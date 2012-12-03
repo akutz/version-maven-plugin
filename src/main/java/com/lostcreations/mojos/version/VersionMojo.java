575618a6091803da9811b6f521710e26d77ce8bc
akutz <sakutz@gmail.com>
2012-12-03 14:22:09 -0600

    allows specification of version parts via configuration

--- src/main/java/com/lostcreations/mojos/version/VersionMojo.java
+++ src/main/java/com/lostcreations/mojos/version/VersionMojo.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Copyright (c) 2012, Schley Andrew Kutz All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * - Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- *
+ * 
  * - Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *
+ * 
  * - Neither the name of the Schley Andrew Kutz nor the names of its
  * contributors may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -50,6 +50,34 @@ public class VersionMojo extends AbstractMojo
      * @component
      */
     MavenProject project;
+
+    /**
+     * Sets the MAJOR version component of the produced version string.
+     * 
+     * @parameter expression="${version.major}"
+     */
+    String versionMajor;
+
+    /**
+     * Sets the MINOR version component of the produced version string.
+     * 
+     * @parameter expression="${version.minor}"
+     */
+    String versionMinor;
+
+    /**
+     * Sets the BUILD version component of the produced version string.
+     * 
+     * @parameter expression="${version.build}"
+     */
+    String versionBuild;
+
+    /**
+     * Sets the REVISION version component of the produced version string.
+     * 
+     * @parameter expression="${version.revision}"
+     */
+    String versionRevision;
 
     /**
      * The name of the variable to hold the MAJOR part of the version.
@@ -136,6 +164,27 @@ public class VersionMojo extends AbstractMojo
         try
         {
             Version v = Version.parse(this.project.getVersion());
+
+            // See if any of the forced version parts are set.
+            if (StringUtils.isNotEmpty(this.versionMajor))
+            {
+                v.setMajor(this.versionMajor);
+            }
+
+            if (StringUtils.isNotEmpty(this.versionMinor))
+            {
+                v.setMinor(this.versionMinor);
+            }
+
+            if (StringUtils.isNotEmpty(this.versionBuild))
+            {
+                v.setBuild(this.versionBuild);
+            }
+
+            if (StringUtils.isNotEmpty(this.versionRevision))
+            {
+                v.setRevision(this.versionRevision);
+            }
 
             if (StringUtils.isNotEmpty(v.getMajor()))
             {
--- src/test/java/com/lostcreations/mojos/version/VersionMojoTest.java
+++ src/test/java/com/lostcreations/mojos/version/VersionMojoTest.java
@@ -1,20 +1,20 @@
 /*******************************************************************************
  * Copyright (c) 2012, Schley Andrew Kutz All rights reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- *
+ * 
  * - Redistributions of source code must retain the above copyright notice, this
  * list of conditions and the following disclaimer.
- *
+ * 
  * - Redistributions in binary form must reproduce the above copyright notice,
  * this list of conditions and the following disclaimer in the documentation
  * and/or other materials provided with the distribution.
- *
+ * 
  * - Neither the name of the Schley Andrew Kutz nor the names of its
  * contributors may be used to endorse or promote products derived from this
  * software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -134,14 +134,14 @@ public class VersionMojoTest
             .getProperties()
             .getProperty(this.mojo.versionStringRaw4VarName));
     }
-    
+
     @Test
     public void executeTestThree() throws Exception
     {
         this.mojo.project.setVersion("a1.0.1-SNAPSHOT");
 
         this.mojo.execute();
-        
+
         Assert.assertEquals(
             "a",
             this.mojo.project.getProperties().getProperty(
@@ -175,6 +175,50 @@ public class VersionMojoTest
             .getProperty(this.mojo.versionString4VarName));
 
         Assert.assertEquals("a1.0.1.0-SNAPSHOT", this.mojo.project
+            .getProperties()
+            .getProperty(this.mojo.versionStringRaw4VarName));
+    }
+
+    @Test
+    public void executeTestFour() throws Exception
+    {
+        this.mojo.project.setVersion("a1.0.1-SNAPSHOT");
+        this.mojo.project.getProperties().setProperty("version.build", "250");
+
+        this.mojo.execute();
+
+        Assert.assertEquals(
+            "a",
+            this.mojo.project.getProperties().getProperty(
+                this.mojo.versionPrefixVarName));
+
+        Assert.assertEquals(
+            "1",
+            this.mojo.project.getProperties().getProperty(
+                this.mojo.versionMajorVarName));
+
+        Assert.assertEquals(
+            "0",
+            this.mojo.project.getProperties().getProperty(
+                this.mojo.versionMinorVarName));
+
+        Assert.assertEquals("250", this.mojo.project
+            .getProperties()
+            .getProperty(this.mojo.versionBuildVarName));
+
+        Assert.assertEquals("-SNAPSHOT", this.mojo.project
+            .getProperties()
+            .getProperty(this.mojo.versionSuffixVarName));
+
+        Assert.assertEquals("1.0.250", this.mojo.project
+            .getProperties()
+            .getProperty(this.mojo.versionStringVarName));
+
+        Assert.assertEquals("1.0.250.0", this.mojo.project
+            .getProperties()
+            .getProperty(this.mojo.versionString4VarName));
+
+        Assert.assertEquals("a1.0.250.0-SNAPSHOT", this.mojo.project
             .getProperties()
             .getProperty(this.mojo.versionStringRaw4VarName));
     }
